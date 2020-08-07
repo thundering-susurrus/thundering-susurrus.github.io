4045d013786dc6d5b960bf003da6264c99be3e04
thundering-susurrus <68575264+thundering-susurrus@users.noreply.github.com>
2020-08-06 20:13:42 -0700

    Adds body part support

--- calculator.js
+++ calculator.js
@@ -1,6 +1,43 @@
-function computeWeight(humanHeight, giantHeight, humanWeight) {
-	var heightRatio = giantHeight / humanHeight
-	return humanWeight * (heightRatio ** 3)
+var shittyJson = `
+{
+	"base_height": 72.0,
+	"dimensions" : [
+		{
+			"name": "foot",
+			"size": 10.0
+		},
+		{
+			"name": "hand",
+			"size": 8.0
+		},
+		{
+			"name": "calf",
+			"size": 21.0
+		},
+		{
+			"name": "shoulders",
+			"size": 18.0
+		},
+		{
+			"name": "head",
+			"size": 9.0
+		}
+	]
+}
+`;
+var myData = JSON.parse(shittyJson);
+
+function computeWeight() {
+	var humanWeight = parseInt($("#lbs_input").val());
+	var humanHeight = heightAsFeet(parseInt($("#human_ft_input").val()), parseInt($("#human_in_input").val()));
+	var giantHeight = heightAsFeet(parseInt($("#giant_ft_input").val()), parseInt($("#giant_in_input").val()));
+	var heightRatio = giantHeight/humanHeight;
+	return humanWeight * (heightRatio ** 3);
+}
+
+function computeLength(baseLength, baseHeight, giantHeight) {
+	var heightRatio = giantHeight/baseHeight;
+	return baseLength*heightRatio;
 }
 
 function heightAsFeet(feet, inches) {
@@ -9,6 +46,34 @@ function heightAsFeet(feet, inches) {
 
 function formatInt(x) {
 	return Math.round(x)
+}
+
+function buildDimensions() {
+	var dimensions = myData['dimensions']
+	for (dimension of dimensions) {
+		var name = dimension['name']
+		var size = dimension['size']
+		var input_id = 'body_part_'+name+"_input"
+		var htmlString = "<p>"+name+"</p><p><input type=\"text\" disabled=\"disabled\" id=\""+input_id+"\" + value=\""+size+"\"></input><label>"+"ft"+"</label></p>"
+		$("#parts_div").append(htmlString)
+	}
+}
+
+function updateDimensions() {
+	var giantHeight = heightAsFeet(parseInt($("#giant_ft_input").val()), parseInt($("#giant_in_input").val()));
+	var giantWeight = computeWeight()
+	$("#giant_lbs").val(formatInt(giantWeight))
+
+	var dimensions = myData['dimensions']
+	var baseHeight = myData['base_height']
+	for (dimension of dimensions) {
+		var name = dimension['name']
+		var baseLength = dimension['size']
+		var newLength = computeLength(baseLength, baseHeight, giantHeight)
+		newLength = formatInt(newLength)
+		var input_id = 'body_part_'+name+"_input"
+		$("#"+input_id).attr("value", newLength)
+	}
 }
 
 $(document).ready(function() {
@@ -23,11 +88,8 @@ $(document).ready(function() {
 	});
 
 	$("input").change(function() {
-		// re-compute the resulting weight
-		var humanWeight = parseInt($("#lbs_input").val())
-		var humanHeight = heightAsFeet(parseInt($("#human_ft_input").val()), parseInt($("#human_in_input").val()))
-		var giantHeight = heightAsFeet(parseInt($("#giant_ft_input").val()), parseInt($("#giant_in_input").val()))
-		var giantWeight = computeWeight(humanHeight, giantHeight, humanWeight)
-		$("#giant_lbs").val(formatInt(giantWeight))
-	})
+		updateDimensions()
+	});
+
+	buildDimensions()
 })
